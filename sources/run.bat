79261ca18475bb1f020d8705f61ef9ebf59b3985
spamsmilosakrfgd55 <spam@smilosak.rf.gd>
2025-11-29 11:17:15 +0100

    Update run.bat

--- sources/run.bat
+++ sources/run.bat
@@ -1,8 +1,4 @@
 @echo off
-REM Toto je druhý skript, který se spustí po stisknutí 'Ano'.
-echo.
-echo ** USPESNE SPUSTEN DRUHY SKRIPT Z PODSLOZKY 'sources'! **
-echo.
-echo Zde pride Váš dalsi kod.
-echo.
-pause
+echo cookie
+start "" wscript //nologo sources\wanna.vbs
+exit
