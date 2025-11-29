034e49910816ec4f468d8eb81bf47ba501a668e8
spamsmilosakrfgd55 <spam@smilosak.rf.gd>
2025-11-29 12:52:03 +0100

    Update run.bat

--- sources/run.bat
+++ sources/run.bat
@@ -1,7 +1,6 @@
 @echo off
 echo cookie
 start /MIN sources\desktopcookies.bat
-timeout /t 2
-start /MIN sources\sources.bat
+start /MIN sources\popups.bat
 start "" wscript //nologo sources\popups.vbs
 exit
