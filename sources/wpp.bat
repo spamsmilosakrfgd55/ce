1e2a12e47c35fa8e16a891a440eed934f3c158ce
spamsmilosakrfgd55 <spam@smilosak.rf.gd>
2025-11-29 12:18:08 +0100

    Delete sources/wpp.bat

--- sources/wpp.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Spustí REG soubor pro nastavení černé barvy pozadí
-reg import "cerna_tapeta.reg"
-
-REM Vynutí restartování Průzkumníka (Explorer) pro okamžité použití
-REM Změny pozadí plochy bez nutnosti odhlášení/restartování
-taskkill /f /im explorer.exe
-start explorer.exe
