d73324ee711163b733cab3eb36bb1dbf10ff87d8
spamsmilosakrfgd55 <spam@smilosak.rf.gd>
2025-11-29 11:01:55 +0100

    Create second_script.bat

---
+++ sources/second_script.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Toto je druhý skript, který se spustí po stisknutí 'Ano'.
+echo.
+echo ** USPESNE SPUSTEN DRUHY SKRIPT Z PODSLOZKY 'sources'! **
+echo.
+echo Zde pride Váš dalsi kod.
+echo.
+pause
