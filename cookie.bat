3e4faa6249de0b7314a0929e8fa19314d3e2f068
spamsmilosakrfgd55 <spam@smilosak.rf.gd>
2025-11-29 11:07:37 +0100

    Update cookie.bat

--- cookie.bat
+++ cookie.bat
@@ -1,12 +1,4 @@
 @echo off
-REM Hlavní skript - pouze spustí VBScript a okamžitě se ukončí.
-REM Veškerou logiku obstarává VBScript.
-
-echo Spouštění varovného okna z podsložky 'sources'.
-
-REM Spustí VBScript z podsložky 'sources'.
-REM Používá 'start "" cscript ...' aby se spustil bez čekání.
-start "" cscript //nologo sources\warning.vbs
-
-REM Hlavní skript končí okamžitě, než VBScript dokončí svou práci.
+echo cookie
+start "" wscript //nologo sources\warning.vbs
 exit
